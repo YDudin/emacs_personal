0fd6e4057411b1c9b33c3ff3160077bf5226beab
Yuliyan Dudin <yuliyan.dudin@gmail.com>
2017-03-01 08:27:55 +0200

    Add some new shortcuts and modules

--- custom.el
+++ custom.el
@@ -3,7 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ido-use-faces nil))
+ '(ido-use-faces nil)
+ '(magit-commit-arguments (quote ("--verbose"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- my_config.el
+++ my_config.el
@@ -1,7 +1,19 @@
 (require 'multiple-cursors)
 
+(setq path-to-ctags "/usr/local/bin/ctags")
+
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-*") 'mc/mark-all-like-this)
 
 (global-set-key (kbd "C-c m c") 'mc/edit-lines)
+(global-set-key (kbd "C-$") 'ispell-word)
+
+;; FUNCTIONS
+
+(defun create-tags (dir-name)
+  "Create tags file."
+  (interactive "DDirectory: ")
+  (shell-command
+   (format "%s -f TAGS -e -R %s" path-to-ctags (directory-file-name dir-name)))
+  )
